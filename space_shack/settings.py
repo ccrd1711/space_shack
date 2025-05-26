0f6cb0b849b74a59ca84714f3acd0ff528dd8665
ccrd1711 <ccrd17@gmail.com>
2025-05-26 11:02:06 +0100

    Add secret key to env.py and settings.py

--- space_shack/settings.py
+++ space_shack/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ygzi0-0k9(l5@@z(5fbq7r!d#vs=ye1vufehk%%$ni9zgr*6p&'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
