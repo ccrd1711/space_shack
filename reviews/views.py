66836f2e98f76964a51f7718c067b44d64cf8a26
ccrd1711 <ccrd17@gmail.com>
2025-05-27 09:32:25 +0100

    create Review Post, connect view and template

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ReviewPost
 
-# Create your views here.
+# Test route view
 def my_blog(request):
     return HttpResponse("Hello, space blog!")
+
+# Actual blog view
+def review_list(request):
+    reviews = ReviewPost.objects.all()
+    return render(request, 'reviews/review_list.html', {'reviews': reviews})
