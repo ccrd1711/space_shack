bc7f409496202b9368da6795f5d71b1289c27b6f
ccrd1711 <ccrd17@gmail.com>
2025-05-28 19:54:25 +0100

    Add real signup page with signupform in accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth import login 
 
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')  # Redirect to login page after successful signup
+            user = form.save()
+            login(request, user)  # Logs the user in after signup
+            return redirect('review_list')  # or wherever you want to send them
     else:
-        form = UserCreationForm()
-    
-    return render(request, 'registration/signup.html', {'form': form})
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
--- space_shack/urls.py
+++ space_shack/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),  # For login/logout#
     path('accounts/', include('accounts.urls')),  # For user registration#
     path('reviews/', include('reviews.urls')),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'), 
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
