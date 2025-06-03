0d26b0aaf0889a7d93def122abcfa93bc7515c77
ccrd1711 <ccrd17@gmail.com>
2025-06-03 08:50:26 +0100

    Add further styling to css. Testing deployment issue where css is not talking to server so collectstatic must be run every time to update testing changes, settings.py amended to rectify unsuccessfully. staticfiles deleted twice and recollected

--- space_shack/settings.py
+++ space_shack/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',#
+    'django.contrib.staticfiles',
     'reviews.apps.ReviewsConfig',
     'whitenoise.runserver_nostatic', 
     'cloudinary',
@@ -142,13 +142,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 #REMOVE BEFORE DEPLOYMENT
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
     os.path.join(BASE_DIR, 'static'), 
 ]
+
+# Disable caching of static files - testing deployment issue
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
