4aee8109d05f712d81076f4e29b15766d1a2cbff
ccrd1711 <ccrd17@gmail.com>
2025-06-05 12:17:17 +0100

    Add logout and in button to index to test - all working, just needs stylging to reposition. Routing tested and working.

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import ReviewForm
-from django.contrib.auth.decorators import login_required 
+from django.contrib.auth.decorators import login_required
 from .forms import CommentForm
 from django.shortcuts import get_object_or_404, redirect
 from .models import ReviewPost, Like
