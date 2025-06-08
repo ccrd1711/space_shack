b19e8f483fbe5c1cbf8cbe3e7710233b3c67f94d
ccrd1711 <ccrd17@gmail.com>
2025-06-08 12:33:15 +0100

    Add additional info to about page for better immersion, add styling, add better views but through the post reviews panel itself rather than code, update slug to allow for more text content in reviews post

--- reviews/models.py
+++ reviews/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 # Create your models here.
 class ReviewPost(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(unique=True, blank=True)
+    slug = models.SlugField(unique=True, blank=True, max_length=200)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
