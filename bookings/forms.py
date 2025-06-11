3d87c255e477c0530b7112866bf69631b8a96f25
ccrd1711 <ccrd17@gmail.com>
2025-06-11 11:43:49 +0100

    Further extensive experimenting and file change to style.css as small laptop screens showing issues on reviews page.

--- bookings/forms.py
+++ bookings/forms.py
@@ -15,7 +15,7 @@ class BookingForm(forms.ModelForm):
         }
 
      # Allow only common email domains
-     def clean_email(self):
+    def clean_email(self):
         email = self.cleaned_data.get('email') 
         if not re.match(r".+@.+\.(com|net|org|io|co\.uk|gov|edu)$", email.lower()):
             raise ValidationError("Please enter a valid email domain (e.g. .com, .org).")
