4f3e144ed301a8990c622d7fc82eb21dafd59a3f
ccrd1711 <ccrd17@gmail.com>
2025-06-11 19:36:53 +0100

    settings.py amendment further to allow site to be loaded in iframes

--- space_shack/settings.py
+++ space_shack/settings.py
@@ -163,3 +163,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+X_FRAME_OPTIONS = 'ALLOWALL'
